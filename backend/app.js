532cf79a4ff6a239aef0a38542652df9dbe4d6b8
dd-thuan <67511121+dd-thuan@users.noreply.github.com>
2022-12-05 23:38:58 +0700

    update

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,13 @@
 const express = require("express");
 const app = express();
-const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
+const cors = require('cors')
 
 // Config
 dotenv.config({ path: "backend/config/config.env" });
 
 app.use(express.json());
-app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors())
 
 // Route Imports
 const user = require("./routes/userRoute");
