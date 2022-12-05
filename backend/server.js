028611011bc97d5d292394d1800a42124e3a28f5
dd-thuan <67511121+dd-thuan@users.noreply.github.com>
2022-12-05 23:34:07 +0700

    update

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,4 @@
 const app = require("./app");
-const cloudinary = require("cloudinary");
-
-const dotenv = require("dotenv");
-const connectDatabase = require("./config/database");
 
 // handling Uncaught Exception
 process.on("uncaughtException", (err) => {
@@ -12,18 +8,9 @@ process.on("uncaughtException", (err) => {
 });
 
 // Config
-if(process.env.NODE_ENV != "PRODUCTION") {
+if (process.env.NODE_ENV != "PRODUCTION") {
   require("dotenv").config({ path: "backend/config/config.env" });
 }
-
-// Connecting to database
-connectDatabase();
-
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
 
 const server = app.listen(process.env.PORT, () => {
   console.log(`"server is working on http://localhost:${process.env.PORT}`);
