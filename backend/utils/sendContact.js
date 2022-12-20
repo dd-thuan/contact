5a49cb0556cb9e56596bf96572a357d2b17bdd27
dd-thuan <67511121+dd-thuan@users.noreply.github.com>
2022-12-20 14:24:20 +0700

    update

--- backend/utils/sendContact.js
+++ backend/utils/sendContact.js
@@ -1,6 +1,6 @@
 const nodeMailer = require("nodemailer");
 
-const sendContact = async (text) => {
+const sendContact = async (text, options) => {
   const transporter = nodeMailer.createTransport({
     host: process.env.SMPT_HOST,
     port: process.env.SMPT_PORT,
@@ -13,7 +13,7 @@ const sendContact = async (text) => {
 
   await transporter.sendMail({
     subject: "CONTACT REQUEST FROM ST UNITED",
-    to: process.env.SMPT_MAIL,
+    to: options.email,
     from: process.env.SMPT_MAIL,
     text,
   });
